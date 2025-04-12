55e2bad2515204b7f87633be71305f2a5988eeed
Cici Tri Fadila <113908200+ceceyeolie@users.noreply.github.com>
2025-04-12 21:45:15 +0700

    Delete js directory

--- js/modules/storage.js
+++
@@ -1,8 +0,0 @@
-export const saveData = (data) => {
-  localStorage.setItem('scheduleData', JSON.stringify(data));
-};
-
-export const loadData = () => {
-  const data = localStorage.getItem('scheduleData');
-  return data ? JSON.parse(data) : [];
-};
